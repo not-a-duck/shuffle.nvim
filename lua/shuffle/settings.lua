efd1beb43fd62f6f65651330d7c56b38e9211708
duck <not-a-duck@github.com>
2021-07-25 19:34:15 +0200

    window border single

--- lua/shuffle/settings.lua
+++ lua/shuffle/settings.lua
@@ -6,9 +6,9 @@ defaults.gveq = true
 -- Window options
 defaults.window_width = 30
 defaults.window_height = 30
-defaults.window_opacity = 30
+defaults.window_opacity = 100
 defaults.window_style = 'minimal'
-defaults.window_border = 'none'
+defaults.window_border = 'single'
 defaults.window_relative = 'win'
 defaults.full_screen = false
 defaults.window_col = 1
