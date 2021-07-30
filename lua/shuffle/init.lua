ed88c1b1ab24721e8d3914ff366305bf663ebd37
duck <not-a-duck@github.com>
2021-07-30 20:59:46 +0200

    ,

--- lua/shuffle/init.lua
+++ lua/shuffle/init.lua
@@ -2,13 +2,13 @@ local methods = require('shuffle.methods')
 
 local plugin = {
   -- Expose some methods as public
-  Reverse = methods.Reverse
-  VReverse = methods.VReverse
-  Shuffle = methods.Shuffle
-  VShuffle = methods.VShuffle
-  Show = methods.Show
-  Hide = methods.Hide
-  setup = methods.Setup
+  Reverse = methods.Reverse,
+  VReverse = methods.VReverse,
+  Shuffle = methods.Shuffle,
+  VShuffle = methods.VShuffle,
+  Show = methods.Show,
+  Hide = methods.Hide,
+  setup = methods.Setup,
 }
 
 return plugin
